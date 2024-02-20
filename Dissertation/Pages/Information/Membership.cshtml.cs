c18f73979f830779bf9e26b9737753a7ed11dda1
ADAM  DRUMMOND <2126671@chester.ac.uk>
2024-02-20 22:21:16 +0000

    created pages

---
+++ Dissertation/Pages/Information/Membership.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dissertation.Pages.Information
+{
+    public class MembershipModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
