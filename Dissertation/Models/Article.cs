b7c9b29bcd93ae347dfca0c39fc0f75337f78741
ADAM  DRUMMOND <2126671@chester.ac.uk>
2024-02-22 23:12:48 +0000

    created news article model

---
+++ Dissertation/Models/Article.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Contracts;
+
+namespace Dissertation.Models
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string? Title { get; set; }
+        [Required]
+        [StringLength(450)]
+        public string? AuthorId { get; set; }
+        [Required]
+        public string? Content { get; set; }
+        public string? Tags { get; set; }
+        public DateTime? PublishDate { get; set; }
+
+    }
+}
