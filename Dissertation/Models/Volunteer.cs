aba92794caf1fb6b1e4175fa8ed8d4fcfa527e29
ADAM  DRUMMOND <2126671@chester.ac.uk>
2024-02-23 01:52:39 +0000

    added volunteer db model

---
+++ Dissertation/Models/Volunteer.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dissertation.Models
+{
+    public class Volunteer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string? Name { get; set; }
+        public string? LinkedInEnding { get; set; }
+        public string? ImgUrl { get; set; }
+        [Required]
+        public string? Level { get; set; }
+        public string? Title { get; set; }
+    }
+}
