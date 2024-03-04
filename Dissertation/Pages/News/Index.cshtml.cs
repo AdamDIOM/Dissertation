b8c3fd8702d675cf8a018b7d8ea3a66a4aa759e6
ADAM  DRUMMOND <2126671@chester.ac.uk>
2024-03-04 00:54:17 +0000

    got tags to work with news page. still requires formatting but functionality works

--- Dissertation/Pages/News/Index.cshtml.cs
+++ Dissertation/Pages/News/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Dissertation.Data;
 using Dissertation.Models;
+using Microsoft.Identity.Client;
 
 namespace Dissertation.Pages.News
 {
@@ -19,11 +20,43 @@ namespace Dissertation.Pages.News
             _context = context;
         }
 
-        public IList<Article> Article { get;set; } = default!;
+        public IList<Article> Article { get; set; } = default!;
+        public IList<ArticleTag> Tags { get; set; } = default!;
+        public IList<ArticleTagLink> TagLinks { get; set; } = default!;
+
+        [BindProperty(SupportsGet = true)]
+        public string? filter { get; set; }
 
         public async Task OnGetAsync()
         {
             Article = await _context.Articles.ToListAsync();
+            Tags = await _context.ArticleTags.ToListAsync();
+            TagLinks = await _context.ArticleTagLinks.ToListAsync();
+            if(filter != null && filter != "")
+            {
+                TagLinks = TagLinks.Where(tl => tl.TagId.ToString() == filter).ToList();
+                Article = Article.Where(a => TagLinks.FirstOrDefault(tl => tl.ArticleId == a.Id) != null).ToList();
+            }
+        }
+
+        public IActionResult OnPostFilter(string f)
+        {
+            filter = f;
+            /*if (qry != null && qry != "")
+            {
+                return Redirect($"View/?qry={qry}&filter={filter}");
+            }*/
+            return Redirect($"News/?filter={filter}");
+        }
+
+        public IActionResult OnPostClearFilter()
+        {
+            filter = "";
+            /*if (qry != null && qry != "")
+            {
+                return Redirect($"View/?qry={qry}");
+            }*/
+            return Redirect($"News");
         }
     }
 }
