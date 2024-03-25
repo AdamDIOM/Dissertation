9f87d1944c3bb3bb7fb2e8c73941d8d4c57da0a5
ADAM  DRUMMOND <2126671@chester.ac.uk>
2024-03-25 04:48:49 +0000

    removed sponsor image from database

--- Dissertation/Migrations/20240325040759_remove-sponsor-image.Designer.cs
+++ Dissertation/Migrations/20240325040759_remove-sponsor-image.Designer.cs
@@ -4,6 +4,7 @@ using Dissertation.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Dissertation.Migrations
 {
     [DbContext(typeof(DissertationContext))]
-    partial class DissertationContextModelSnapshot : ModelSnapshot
+    [Migration("20240325040759_remove-sponsor-image")]
+    partial class removesponsorimage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Dissertation/Migrations/20240325040759_remove-sponsor-image.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dissertation.Migrations
+{
+    /// <inheritdoc />
+    public partial class removesponsorimage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageData",
+                table: "Sponsor");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ImageData",
+                table: "Sponsor",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+    }
+}
