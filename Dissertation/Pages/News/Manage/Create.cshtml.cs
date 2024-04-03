5d971e114596bf4f29ad2dcf1760517688fbe491
ADAM  DRUMMOND <2126671@chester.ac.uk>
2024-04-03 19:27:52 +0100

    updated news article authors to autofill from logged in user

--- Dissertation/Pages/News/Manage/Create.cshtml.cs
+++ Dissertation/Pages/News/Manage/Create.cshtml.cs
@@ -12,24 +12,34 @@ using System.Drawing;
 using Microsoft.EntityFrameworkCore;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Dissertation.Pages.News.Manage
 {
     public class CreateModel : PageModel
     {
         private readonly Dissertation.Data.DissertationContext _context;
+        private readonly UserManager<SiteUser> _userManager;
         private readonly IConfiguration _config;
 
-        public CreateModel(Dissertation.Data.DissertationContext context, IConfiguration config)
+        public CreateModel(Dissertation.Data.DissertationContext context, IConfiguration config, UserManager<SiteUser> userManager)
         {
             _context = context;
             _config = config;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> OnGetAsync()
         //public IActionResult OnGet()
         {
             Tags = await _context.ArticleTags.ToListAsync();
+
+            var u = await _userManager.GetUserAsync(User) ?? new SiteUser { Email = "" };
+            var email = await _userManager.GetEmailAsync(u);
+            Volunteer? user = null;
+            if (email != null) user = _context.Volunteer.FirstOrDefault(v => v.Email == email);
+            Author = email ?? "";
+            if (user != null && user.Name != null) Author = user.Name;
             return Page();
         }
 
@@ -39,6 +49,8 @@ namespace Dissertation.Pages.News.Manage
         public IList<ArticleTag> Tags { get; set; } = default!;
         [BindProperty]
         public bool HomepageDisplay { get; set; } = default!;
+        [BindProperty]
+        public string Author { get; set; } = default!;
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
