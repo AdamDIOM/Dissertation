390e47ceb3923b5d874368972192f650e4d81bd2
ADAM  DRUMMOND <2126671@chester.ac.uk>
2024-04-04 16:47:14 +0100

    added show/hide on navbar to tag manager and return to news button

--- Dissertation/Pages/News/Manage/Tags/Index.cshtml.cs
+++ Dissertation/Pages/News/Manage/Tags/Index.cshtml.cs
@@ -25,5 +25,78 @@ namespace Dissertation.Pages.News.Manage.Tags
         {
             ArticleTag = await _context.ArticleTags.ToListAsync();
         }
+
+        public async Task<IActionResult> OnPostShowAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tag = await _context.ArticleTags.FirstOrDefaultAsync(t => t.Id == id);
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            tag.NavDisplay = true;
+            _context.Attach(tag).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TagExists(tag.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        public async Task<IActionResult> OnPostHideAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tag = await _context.ArticleTags.FirstOrDefaultAsync(t => t.Id == id);
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            tag.NavDisplay = false;
+            _context.Attach(tag).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TagExists(tag.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool TagExists(int id)
+        {
+            return _context.Articles.Any(e => e.Id == id);
+        }
     }
 }
