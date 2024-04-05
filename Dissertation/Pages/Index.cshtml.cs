c7be3880bce131769cba015ec6e5b814c21ff434
ADAM  DRUMMOND <2126671@chester.ac.uk>
2024-04-05 21:40:46 +0100

    modified homepage carousel to pull images from the database list

--- Dissertation/Pages/Index.cshtml.cs
+++ Dissertation/Pages/Index.cshtml.cs
@@ -15,11 +15,17 @@ namespace Dissertation.Pages
             _context = context;
         }
         public IList<Article> Article { get; set; } = default!;
+        public IList<Image> Image { get; set; } = default!;
+        public Image ActiveImage { get; set; } = default!;
         public async Task OnGetAsync()
         {
             Article = await _context.Articles.Where(a => a.PublishDate < DateTime.Now && (a.HomepageDisplay ?? false)).ToListAsync();
             
             Article = Article.OrderByDescending(a => a.PublishDate).Take(3).ToList();
+
+            Image = await _context.Image.Where(i => i.HomepageBannerDisplay ?? false).ToListAsync();
+            ActiveImage = Image.First();
+            Image.Remove(ActiveImage);
         }
     }
 }
