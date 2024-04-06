cc1aa083c1fd0a7752415613ee331315405603f1
ADAM  DRUMMOND <2126671@chester.ac.uk>
2024-04-06 17:02:08 +0100

    removed ability for volunteers to delete images other than their own (excl. admins)

--- Dissertation/Pages/About/Gallery/Manage/Index.cshtml.cs
+++ Dissertation/Pages/About/Gallery/Manage/Index.cshtml.cs
@@ -7,22 +7,37 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Dissertation.Data;
 using Dissertation.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Dissertation.Pages.About.Gallery.Manage
 {
     public class IndexModel : PageModel
     {
         private readonly Dissertation.Data.DissertationContext _context;
+        private readonly UserManager<SiteUser> _userManager;
 
-        public IndexModel(Dissertation.Data.DissertationContext context)
+        public IndexModel(Dissertation.Data.DissertationContext context, UserManager<SiteUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
+        [BindProperty]
+        public string CurrentUser { get; set; } = default!;
+        [BindProperty]
+        public bool AdminUser { get; set; } = default!;
 
         public IList<Image> Image { get;set; } = default!;
 
         public async Task OnGetAsync()
         {
+            var u = await _userManager.GetUserAsync(User) ?? new SiteUser { Email = "" };
+            var email = await _userManager.GetEmailAsync(u);
+            Volunteer? user = null;
+            if (email != null) user = _context.Volunteer.FirstOrDefault(v => v.Email == email);
+            CurrentUser = email ?? "Volunteer";
+            if (user != null && user.Name != null) CurrentUser = user.Name;
+
+            AdminUser = (user != null && (await _userManager.IsInRoleAsync(u, "Admin")));
             Image = await _context.Image.ToListAsync();
         }
     }
