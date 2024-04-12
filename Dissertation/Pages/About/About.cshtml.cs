dec1875360ab0542f96f50b37107e68ed788e47a
ADAM  DRUMMOND <2126671@chester.ac.uk>
2024-04-12 01:14:10 +0100

    add about us page content

--- Dissertation/Pages/About/About.cshtml.cs
+++ Dissertation/Pages/About/About.cshtml.cs
@@ -5,8 +5,28 @@ namespace Dissertation.Pages.About
 {
     public class AboutModel : PageModel
     {
+        public int Age { get; set; }
+
+        public AboutModel() { 
+            Age = DateTime.Now.Year - 2014;
+        }
         public void OnGet()
         {
+            Age = DateTime.Now.Year - 2014;
+        }
+
+        public string Ordinal(int year)
+        {
+            var ones = year % 10;
+            var tens = Math.Floor(year / 10f) % 10;
+            if (tens == 1) return $"{year}th";
+            switch (ones)
+            {
+                case 1: return $"{year}st";
+                case 2: return $"{year}nd";
+                case 3: return $"{year}rd";
+                default: return $"{year}th";
+            }
         }
     }
 }
